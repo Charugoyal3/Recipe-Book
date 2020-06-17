0a7370207d0ad9641277b9195a8b205af5f093b1
Charugoyal3 <charugoyal180@gmail.com>
2020-06-17 17:36:55 +0530

    added user grup

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,3 +5,34 @@ exports.home = function (req, res) {
     res.render('home')
 
 }
+exports.signup = function (req, res) {
+
+    User.signup(
+        new User({
+            userName: req.body.username,
+            email: req.body.email,
+            picture: req.body.picture,
+            biography: req.body.bio
+        }),
+        req.body.password,
+        function (err, user) {
+            if (err) {
+                console.log(err);
+                return res.render("signup");
+            }
+
+        }
+    );
+
+}
+
+exports.signin = function (req, res) {
+
+    res.render('signin')
+
+}
+
+exports.logout = function (req, res) {
+    req.logout();
+    res.redirect("/home");
+};
--- router/userRouter.js
+++ router/userRouter.js
@@ -3,5 +3,10 @@ const router = express.Router()
 const userController = require('../controllers/userController')
 
 router.get('/', userController.home)
+router.get('/signin', userController.signin)
+router.get('/signup', userController.signup)
+router.post('/user', userController.add)
+router.get('/logout', userController.add)
+
 
 module.exports = router
