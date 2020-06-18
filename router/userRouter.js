14d63a2b89c2c30e60a2254821202fbaadca03ef
Charugoyal3 <charugoyal180@gmail.com>
2020-06-18 15:30:11 +0530

    category controllers

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,4 +1,4 @@
-const express = require('express').Router();
+const router = require('express').Router();
 const {
     getUsers,
     postUsers,
--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require("express");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+
+const userRouter = require("./router/userRouter");
+
+const categoryRouter = require("./router/categoryRouter");
 
 const recipeRouter = require("./router/recipeRouter");
+
 
 // for recipe
 app.use("/:userId/", recipeRouter);
@@ -12,8 +17,12 @@ app.use("/:userId/", recipeRouter);
 //for users
 app.use("/users/", userRouter);
 
+//for category
+app.use("/category/", categoryRouter);
+
+
 app.get("*", (req, res) => {
-  res.send("app started!");
+  res.send("App started!");
 });
 
 app.listen(PORT, () => {
